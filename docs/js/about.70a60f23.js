8c28bdda9132a314407e0784b860bbea00e48e19
tonovarela <tonovarela@live.com>
2022-05-18 12:37:26 -0500

    docs

---
+++ docs/js/about.70a60f23.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmaps_app"]=self["webpackChunkmaps_app"]||[]).push([[443],{242:function(a,n,s){s.r(n),s.d(n,{default:function(){return f}});var u=s(252);const t={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),r=[e];function p(a,n){return(0,u.wg)(),(0,u.iD)("div",t,r)}var c=s(744);const i={},o=(0,c.Z)(i,[["render",p]]);var f=o}}]);
+//# sourceMappingURL=about.70a60f23.js.map
